892c0cb71ba1dbb4950ec3ea9ee4beb4aba60272
Ahmedin Hasanović <ahasanovic2@etf.unsa.ba>
2023-05-14 11:08:18 +0200

    Added menus
    Naslovna strana, dugme za meni pokazuje opcije, login i signup pokazuju opcije

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import Appbar from "./components/Appbar";
-import Korisnik from "./components/Korisnik";
+import { BrowserRouter as Routes, Router, Route, Link, Outlet } from 'react-router-dom';
+import Appbar from "./notLogged/Appbar";
+import HomePage from "./notLogged/Homepage";
+import Korisnik from "./notLogged/Korisnik";
+import AppbarLoggedIn from "./userLoggedIn/AppbarLoggedIn";
+import LoginPage from './notLogged/Login';
 
 function App() {
   return (
     <div className="App">
-      <Appbar />
-      <Korisnik />
+      <Appbar/>
+      <HomePage/>
     </div>
   );
 }
---
+++ frontend/src/notLogged/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    // Add login logic here
+    console.log('Logging in...');
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
